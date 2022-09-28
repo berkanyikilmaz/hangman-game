58115c78e8363bae64377f07faa77418cc571daf
Berkan Yıkılmaz <berkan.yikilmaz@ug.bilkent.edu.tr>
2022-09-28 15:17:18 +0300

    initialize project files

---
+++ Hangman.java
@@ -0,0 +1,54 @@
+public class Hangman {
+
+    private StringBuffer SecretWord;
+    private StringBuffer allLetters;
+    private StringBuffer unusedLetters;
+    private StringBuffer usedLetters;
+    private int numberOfIncorrectTries;
+    private int maxAllowedIncorrectTries;
+    private StringBuffer knownSoFar;
+
+    public Hangman() {
+
+    }
+
+    public String getAllLetters() {
+
+    }
+
+    public String getUsedLetters() {
+        
+    }
+
+    public String getUnusedLetters() {
+        
+    }
+
+    public int getNumOfIncorrectTries() {
+        
+    }
+
+    public int getMaxAllowedIncorrectTries() {
+        
+    }
+
+    public String getKnownSoFar() {
+        
+    }
+
+    public int tryThis( /*letter */) {
+
+    }
+
+    public boolean isGameOver() {
+
+    }
+
+    public boolean hasLost() {
+
+    }
+
+    private void chooseSecretWord() {
+        
+    }
+}
