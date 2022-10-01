8903f424b44800f0d62a73024932b5c68dfa2b35
Mustafa Özkan İr <mustafa54ir@gmail.com>
2022-10-01 06:35:17 +0300

    Changes in HangmanApp and Hangman Classes
    I fixed some syntax errors in methods and added some new functional methods to the Hangman class. I also almost completed the HangmanApp class so that it can work properly, but it still needs some further modification.

--- Hangman.java
+++ Hangman.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 public class Hangman {
 
-    private StringBuffer SecretWord;
+    private StringBuffer secretWord;
     private StringBuffer allLetters;
     private StringBuffer unusedLetters;
     private StringBuffer usedLetters;
@@ -9,69 +11,72 @@ public class Hangman {
     private StringBuffer knownSoFar;
     int count = (int)Math.random()*12;
 
-    public Hangman() {
-        numberOfIncorrectTries = 0;
+    //Constructor
+    public Hangman (){
+        this.numberOfIncorrectTries = 0;
         maxAllowedIncorrectTries = 6;
         chooseSecretWord();
+        this.allLetters = new StringBuffer("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        this.unusedLetters = new StringBuffer("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        this.usedLetters = new StringBuffer("");
+        this.knownSoFar = new StringBuffer();
+        hideSecretWord(this.secretWord);
     }
 
     public String getAllLetters() {
-        return allLetters;
+        return this.allLetters.toString();
     }
 
     public String getUsedLetters() {
-        return usedLetters;        
+        return this.usedLetters.toString();        
     }
 
     public String getUnusedLetters() {
-        return unusedLetters;
-        
+        return this.unusedLetters.toString();
     }
 
     public int getNumOfIncorrectTries() {
-        return numberOfIncorrectTries;
+        return this.numberOfIncorrectTries;
     }
 
     public int getMaxAllowedIncorrectTries() {
-        return maxAllowedIncorrectTries;        
-
-        
+        return this.maxAllowedIncorrectTries;        
     }
 
     public String getKnownSoFar() {
-        return knownSoFar;
-        
+        return this.knownSoFar.toString();
     }
 
-    public int tryThis( char c1) {
-        usedLetters.append(letter);
-        
+    public StringBuffer getSecretWord(){
+        return this.secretWord;
+    }
 
-        int occurences = 0;
-        for (int i = 0; i < secretWord.length(); i++) {
-            if (secretWord.charAt(i) == letter) {
-                occurences++;
-                knownSofar.insert(i, letter);
-                knownSofar.setLength(secretWord.length());
-            } 
-                
-
+    //Method to hide the secretWord.
+    public void hideSecretWord( StringBuffer secretWord ){
+        for ( int i = 0; i < secretWord.length(); i++ ){
+            this.knownSoFar.replace(i, i+1, "_");
         }
-        if (occurences == 0)
-            numberOfIncorrectTries++;
-        return occurences;
-              
+    }
+
+    //Method to return the number of occurences of a letter in secretWord
+    public int tryThis ( String letter ) {
+        int result = 0;
+        for ( int i = 0; i < this.secretWord.length(); i++ ){
+            if( Character.toString(this.secretWord.charAt(i)).equals(letter)  ) {
+                result++;
+            }
+        }
+        if (result == 0) this.numberOfIncorrectTries++;
+        return result;
     }
 
     public boolean isGameOver() {
-        if (numberOfIncorrectTries > maxAllowedIncorrectTries ||getKnownSofar().equals(secretWord.toString()))
-        return true;
+        if (numberOfIncorrectTries > maxAllowedIncorrectTries || getKnownSoFar().equals(secretWord.toString()))
+            return true;
 
-    else
-        return false;
+        else
+            return false;
         }
-
-    
 
     public boolean hasLost() {
         boolean lost = false;
@@ -80,15 +85,71 @@ public class Hangman {
                 lost = true;
         }
         return lost;
-        }
-
-    
-
-    private void chooseSecretWord() {
-        secretWord.insert(0, fixedList[count]);
-        for (int i = 0; i < secretWord.length(); i++) {
-            knownSofar.append('-');
-        }
-        count++;
     }
+    
+    //Method see if the player has won or not.
+    public boolean hasWon(){
+        if (this.secretWord.toString().equals(this.knownSoFar.toString())) return true;
+        else return false;
+    }
+    
+    //Method to choose a secret word from a list of words.
+    public void chooseSecretWord(){
+        this.secretWord = new StringBuffer();
+        String[] secretWords = { "ABRUPTLY", "ABYSS", "AFFIX", "AVENUE", "BEEKEEPER", "COBWEB", "COCKINESS", 
+                                "DISAWOV", "FISHHOOK", "FOXGLOVE", "GAZEBO", "JACKPOT", "KICKSHAW", "KILOBYTE", "PAJAMA", "PIGGYBACK", 
+                                "PNEUMONIA", "SPHINX", "STRETCH", "SUBWAY", "ZIPPER", "ZODIAC", "ZOMBIE" };
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(secretWords.length);
+
+        this.secretWord = this.secretWord.append(secretWords[randomIndex]);
+    }
+    
+    //Method to reveal known letters
+    public void updateKnownSoFar(String letter){
+        for ( int i = 0; i < this.secretWord.length(); i++ ){
+            if( Character.toString(this.secretWord.charAt(i)).equals(letter) ) {
+                knownSoFar.replace(i, i+1, letter);
+            }
+        }
+    }
+
+    //Method to display the number of incorrect guesses of player
+    public String displayTheHangman(){
+        String base = "¯¯¯\n";
+        String pole3 = " |   O\n";
+        String pole1 = " | \n";
+        String pole2 = " |   |\n";
+        String ceiling = "  ___\n";
+        String poleWithLeftArm = " |  /  \n";
+        String poleWithBothArms = " |  / \\\n";
+
+        String result0 = ceiling + pole1 + pole1 + pole1 + pole1 + pole1 + pole1 + base;
+        String result1 = ceiling + pole2 + pole1 + pole1 + pole1 + pole1 + pole1 + base;
+        String result2 = ceiling + pole2 + pole3 + pole1 + pole1 + pole1 + pole1 + base;
+        String result3 = ceiling + pole2 + pole3 + poleWithLeftArm + pole1 + pole1 + pole1 + pole1 + base;
+        String result4 = ceiling + pole2 + pole3 + poleWithBothArms + pole1 + pole1 + pole1 + pole1 + base;
+        String result5 = ceiling + pole2 + pole3 + poleWithBothArms + pole2 + pole1 + pole1 + pole1 + base;
+        String result6 = ceiling + pole2 + pole3 + poleWithBothArms + pole2 + poleWithLeftArm + pole1 + pole1 + base;
+        String result7 = ceiling + pole2 + pole3 +poleWithBothArms + pole2 + poleWithBothArms + pole1 + base;
+        String ultimateResult = result0;
+
+        if( numberOfIncorrectTries == 0 ) ultimateResult = result0;
+        if( numberOfIncorrectTries == 1 ) ultimateResult = result1;
+        if( numberOfIncorrectTries == 2 ) ultimateResult = result2;
+        if( numberOfIncorrectTries == 3 ) ultimateResult = result3;
+        if( numberOfIncorrectTries == 4 ) ultimateResult = result4;
+        if( numberOfIncorrectTries == 5 ) ultimateResult = result5;
+        if( numberOfIncorrectTries == 6 ) ultimateResult = result6;
+        if( numberOfIncorrectTries == 7 ) ultimateResult = result7;
+
+        return ultimateResult;
+    }
+
+    //Method to update unusedLetters and usedLetters 
+    public void updateUsedAndUnusedLetters( String playerGuess ){
+        this.usedLetters.append(playerGuess);
+        this.unusedLetters.delete(unusedLetters.indexOf(playerGuess), unusedLetters.indexOf(playerGuess));
+    }
+
 }
--- HangmanApp.java
+++ HangmanApp.java
@@ -1,9 +1,55 @@
+import java.util.*;
+
 public class HangmanApp {
     
     public static void main(String[] args) {
-        char c3 = 'c';
-        char c4 = 'c';
-        System.out.println(c4 == c3);
+
+        Scanner input = new Scanner(System.in);
+        String playerGuess; 
+
+        System.out.println("Welcome to the Best Hangman Game You've Ever Seen!");
+        System.out.println("You have maximum 6 letter tries, if you exceed the limit you will lose!");
+        System.out.println("Fasten your seatbelts, game is about to start!");
+
+        Hangman current = new Hangman();
+        //System.out.println(current.secretWord);
+
+        while ( !current.isGameOver() ){
+            
+            int occurenceOfLetter = 0;
+
+            System.out.print("Let's try a letter: ");
+            playerGuess = input.nextLine().toUpperCase();
+            while ( current.getUsedLetters().indexOf(playerGuess) >= 0) {
+                System.out.print("You have tried this letter, please try another: ");
+                playerGuess = input.nextLine().toUpperCase();
+            }
+            System.out.println();
+            occurenceOfLetter = current.tryThis(playerGuess);
+            current.updateUsedAndUnusedLetters(playerGuess);
+            if  ( occurenceOfLetter == 0 ){
+                System.out.println("Your letter is not in the secret word, please try again");
+                System.out.println( "Your Secret Word: " + current.getKnownSoFar() );
+                System.out.println("The letters you used: " + current.getUsedLetters());
+                System.out.println(current.displayTheHangman());
+                System.out.println();
+            }
+
+            else if ( occurenceOfLetter > 0 ){
+                System.out.println( "Your guess is corret!");
+                current.updateKnownSoFar(playerGuess);
+                System.out.println( "Your Secret Word: " + current.getKnownSoFar() );
+                System.out.println("The letters you used: " + current.getUsedLetters());
+                System.out.println(current.displayTheHangman());
+                System.out.println();
+            }
+        }
+
+        
+        if (current.hasLost()) System.out.println("You have LOST! Your word was: " + current.getSecretWord() + "\n" + current.displayTheHangman());
+
+        else  System.out.println("You have WON! CONGRATULATIONS!");
+        System.out.println("Game is over! Thank you for playing our game! ");
         
     }
 }
